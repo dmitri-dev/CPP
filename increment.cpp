709cc77bbe58a5ef3c04eafc3a06620ab0f08a65
dmitri-dev <60167177+dmitri-dev@users.noreply.github.com>
2020-11-19 13:37:46 -0500

    Module 8

---
+++ increment.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void increment(int &var, int i = 1) {
+    var += i;
+}
+
+void increment(float &var, int i = 1) {
+    var += float(i);
+}
+
+void increment() {
+    int intvar = 0;
+    float floatvar = 1.5;
+    for(int i = 0; i < 10; i++)
+        if(i % 2) {
+            increment(intvar);
+            increment(floatvar, sqrt(intvar));
+        }
+        else {
+            increment(intvar,i);
+            increment(floatvar);
+        }
+    cout << float(intvar) * floatvar << endl;
+}
